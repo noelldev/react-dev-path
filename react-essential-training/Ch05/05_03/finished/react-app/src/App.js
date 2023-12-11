2a599f474f6a36a6766645d9143de34fdf747af4
kodrr <ntlauigan@gmail.com>
2023-12-11 18:54:14 +0800

    Add chapter 5, create custom hook

---
+++ react-essential-training/Ch05/05_03/finished/react-app/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { useState } from 'react';
+
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+  return [
+    {
+      value,
+      onChange: (e) => setValue(e.target.value),
+    },
+    () => setValue(initialValue),
+  ];
+}
+
+function App() {
+  const [titleProps, resetTitle] = useInput('');
+  const [colorProps, resetColor] = useInput('#000000');
+  console.log(colorProps);
+  const submit = (e) => {
+    e.preventDefault();
+    alert(`${titleProps.value}, ${colorProps.value}`);
+    resetTitle();
+    resetColor();
+  };
+
+  return (
+    <form onSubmit={submit}>
+      <input {...titleProps} type="text" placeholder="color title..." />
+      <input {...colorProps} type="color" />
+      <button>ADD</button>
+    </form>
+  );
+}
+
+export default App;
