c77ac8d1b11dda40ed4a30e35fd0811fbbb757a7
kodrr <ntlauigan@gmail.com>
2023-12-18 20:19:12 +0800

    react-creating-and-hosting-fsa Add ch 04

--- react-creating-and-hosting-fsa/03_05e/my-blog-backend/src/db.js
+++ react-creating-and-hosting-fsa/03_05e/my-blog-backend/src/db.js
@@ -3,13 +3,10 @@ import { MongoClient } from 'mongodb';
 let db;
 
 async function connectToDb(cb) {
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
-    db = client.db('react-blog-db');
-    cb();
+  const client = new MongoClient('mongodb://127.0.0.1:27017');
+  await client.connect();
+  db = client.db('react-blog-db');
+  cb();
 }
 
-export {
-    db,
-    connectToDb,
-};
+export { db, connectToDb };
